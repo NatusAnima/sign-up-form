ab6838e864853bb39a59ce484218b502fd283229
NatusAnima <i.yavets@gmail.com>
2022-10-02 20:55:54 +0300

    Add UI, add some styling, fix image compression on resize

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function validate_password() {
+ 
+    var password = document.getElementById('password');
+    var confirm_password = document.getElementById('confirm_password');
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
